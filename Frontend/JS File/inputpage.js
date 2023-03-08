e1a29fe75521b5e261058ffd81db111350a20bbc
Adwait Shirodkar <adwaitshirodkar239@gmail.com>
2023-03-08 16:33:02 +0530

    Added JS files and some changes in Frontend

---
+++ Frontend/JS File/inputpage.js
@@ -0,0 +1,8 @@
+dropContainer.ondragover = dropContainer.ondragenter = function (evt) {
+    evt.preventDefault();
+};
+dropContainer.ondrop = function (evt) {
+    fileInput.files = evt.dataTransfer.files;
+
+    evt.preventDefault();
+};
