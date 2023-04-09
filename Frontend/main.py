1aee02b1440b94c4f18f316c6782e096811b0e46
Adwait Shirodkar <adwaitshirodkar239@gmail.com>
2023-04-09 20:54:38 +0530

    Added Heirarchial Structure according to Flask app

---
+++ Frontend/main.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+import pandas as pd
+
+app = Flask(__name__)
+app.template_folder = 'templates'
+app.static_folder = 'static'
+
+@app.route("/")
+def hello_world():
+    return render_template("index2.html")
+
+
+@app.route("/storeData", methods=["POST"])
+def store_data():
+    if request.method == "POST":
+        # Get the uploaded CSV file
+        file1 = request.files["transcript"]
+        file2 = request.files["summarization"]
+
+        # Read the CSV file into a pandas DataFrame
+        df1 = pd.read_csv(file1)
+        contents = file2.read()
+
+        # Get the column names
+        t_columns = list(df1.columns)
+     
+
+        # Get the data as a list of dictionaries
+        t_data = df1.to_dict("records")
+       
+
+        # Render the template with the data
+        return render_template(
+            "dashboard.html",
+            t_columns=t_columns,
+            t_data=t_data,
+           content=contents
+        )
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000)
